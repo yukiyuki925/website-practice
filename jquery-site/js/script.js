46b3588828c6c449ac9f6ae1e6ac362a4ac03a66
yukiyuki925 <yuki.work1999@gmail.com>
2024-01-08 14:02:05 +0900

    slider commit

--- jquery-site/js/script.js
+++ jquery-site/js/script.js
@@ -23,3 +23,36 @@ $(function () {
     $(this).next().slideToggle(200);
   });
 });
+
+// スライダー
+$(function () {
+  let count = $("#slide li").length;
+  let current = 1;
+  let next = 2;
+  let interval = 3000;
+  let duration = 500;
+  let timer;
+  $("#slide li:not(:first-child)").hide();
+  timer = setInterval(slideTimer, interval);
+
+  function slideTimer() {
+    $("#slide li:nth-child(" + current + ")").fadeOut(duration);
+    $("#slide li:nth-child(" + next + ")").fadeIn(duration);
+    current = next;
+    next = ++next;
+    if (next > count) {
+      next = 1;
+    }
+
+    $("#button li a").removeClass("target");
+    $("#button li:nth-child(" + current + ") a").addClass("target");
+  }
+
+  $("#button li a").click(function () {
+    next = $(this).html();
+    clearInterval(timer);
+    timer = setInterval(slideTimer, interval);
+    slideTimer();
+    return false;
+  });
+});
